09b23362efc56819ce4ce0051b679fb06508d59f
Chen Yang <yangchen@thinkmore.info>
2021-12-29 10:11:11 -0500

    add more commands

--- git_annex_helper/cmds.py
+++ git_annex_helper/cmds.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import Optional
+from datetime import datetime
 
 from loguru import logger
 import typer
@@ -65,6 +66,49 @@ def detect(file: Path):
         detect_a_file(file)
     elif file.is_dir():
         detect_all_files_under_dir(file)
+
+@app.command()
+def clone(local_name : str, remote_name : str, repo_path : Path ):
+    local_path = Path(repo_path.name).absolute()
+    r = "origin"
+    if remote_name:
+        r = remote_name
+
+    os.system(f"git clone -o {remote_name} {repo_path}")
+    
+    os.system(f"git -C {repo_path.name} annex init {local_name}")
+
+    os.system(f"git -C {repo_path.name} annex sync")
+
+    os.system(f"git -C {repo_path} remote add {local_name} {local_path}")
+
+
+@app.command()
+def init(local_name : str, local_path : Path ):
+    os.system(f"mkdir {local_path}")
+    os.system(f"git -C {local_path} init")
+    os.system(f"git -C {local_path} annex init {local_name}")
+
+
+@app.command()
+def status():
+    os.system(f"git status")
+
+@app.command()
+def sync():
+    os.system(f"git annex sync")
+
+@app.command()
+def list():
+    os.system(f"git annex list")
+
+@app.command()
+def add(path : Path, comment : Optional[str] = typer.Argument(None)):
+    all_comment = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    if comment:
+        all_comment += ":" + comment
+    os.system(f"git annex add {path}")
+    os.system(f"git commit -m '{all_comment}'")
 
 @app.command()
 def help():
